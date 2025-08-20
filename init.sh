cd4b160a81c8e19847ed7895687e85f6205e3aa8
mponsnieto <mpn26141@gmail.com>
2025-08-20 19:06:57 +0200

    optimization2

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ npm init -y
 npm i express cors
 
 node server.js
+
+cd C:\Users\Usuario\OneDrive\Clúster de Transición Ecológica de las Illes Balears\CTEIB - 02 - Actividades\GT INNOVACION\10 - AGROPV Turismo\WP4 - Simulador\6 - Simulador\App online\aterraproject\pvgis-proxy
+node server.js
